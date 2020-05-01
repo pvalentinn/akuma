d8d86f0dc2912ccd01ab0858e45cee474bc67b17
LenGaL <pol.mansart@gmail.com>
2020-05-01 13:16:13 +0200

    restructuration

--- App.js
+++ App.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Init from './components/Init';
-// import Header from './components/Header'
+import Header from './components/Header';
+import LoadingScreen from './components/LoadingScreen';
+import loadPopular from './API/popular';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <Header></Header> */}
-      <Init></Init>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          data: null,
+          isLoaded: false
+      }
+  }
+
+  async componentDidMount() {
+      await this.awaitSetState({data: await loadPopular()});
+      await this.awaitSetState({isLoaded: true});
+  }
+
+  awaitSetState(newChange) {
+      return new Promise( resolve => {
+          this.setState(newChange, () => resolve());
+      });
+  }
+
+  render() {
+    if(!this.state.isLoaded) return <LoadingScreen />
+    else {
+      return (
+        <View style={styles.container}>
+          <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <Header></Header>
+            <Init data={this.state.data}></Init>
+          </ScrollView>
+        </View>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +45,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#27496d',
   },
+  scrollContainer: {
+    height: '120%', 
+    justifyContent: 'space-evenly'
+  }
 });
--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import { View, Image, ScrollView } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 
 export default class LoadingScreen extends Component {
     render() {
         return (
-            <View style={{flex: 1, justifyContent: "center"}}>
-                <Image source={require('../assets/loading.png')} style={{height: '100%', width: 'auto', marginTop: 100}}></Image>
+            <View style={styles.container}>
+                <Image source={require('../assets/loading.png')} style={styles.img}></Image>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        justifyContent: "center", 
+        backgroundColor: '#27496d'
+    },
+    img: {
+        height: '100%', 
+        width: 'auto',
+         marginTop: 250
+    }
+})
